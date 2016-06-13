b62e4804ee92efef40aff7985d34454aaad3bcbf
Clinton Selke <clinuxrulz@gmail.com>
2016-06-13 20:58:01 +1000

    EntityDecl

--- haskell/src/EcsEdsl/Declaration.hs
+++ haskell/src/EcsEdsl/Declaration.hs
@@ -2,19 +2,38 @@
 
 module EcsEdsl.Declaration where
 
+import EcsEdsl.Expr
+
 data Declaration
   = DeclComponentType ComponentTypeDecl
   | DeclEntity EntityDecl
 
+data EntityDecl
+  = EntityDecl
+      [ComponentDecl] -- ^ Components
+
 data ComponentTypeDecl
   = ComponentTypeDecl
-      String      -- ^ Component Type Name
-      [ParamDecl] -- ^ Parameters
+      String          -- ^ Component Type Name
+      [ParamTypeDecl] -- ^ Parameter Types
 
-data EntityDecl
+data ComponentDecl
+  = ComponentDecl
+      String      -- ^ Component Type Name
+      [ParamDecl] -- ^ Parameter Values
 
 data ParamDecl
   = ParamDecl
+      String        -- ^ Name
+      (forall r.
+        (forall t.
+          Expr t    -- ^ Value
+          -> r
+        ) -> r
+      )
+
+data ParamTypeDecl
+  = ParamTypeDecl
       String    -- ^ Name
       TypeDecl  -- ^ Type
 
