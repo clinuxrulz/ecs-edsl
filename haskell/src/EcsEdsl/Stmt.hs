1d7ae5de8cc3cc79303e75cf1930f68c116fe6a3
Clinton Selke <clinuxrulz@gmail.com>
2016-06-13 21:08:16 +1000

    LaterWithEntitySystem

--- haskell/src/EcsEdsl/Stmt.hs
+++ haskell/src/EcsEdsl/Stmt.hs
@@ -3,6 +3,8 @@
 module EcsEdsl.Stmt where
 
 import EcsEdsl.Expr
+import EcsEdsl.EntitySystemEff
 
 data Stmt next
   = Eval (forall r. (forall a. (Expr a, a -> next) -> r) -> r)
+  | LaterWithEntitySystem (EntitySystemEff ()) next
