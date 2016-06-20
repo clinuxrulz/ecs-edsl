14beb7b5408346b5b9297763d8de75b865722a53
Clinton Selke <clinuxrulz@gmail.com>
2016-06-20 20:47:16 +1000

    wip

--- haskell/library/src/EcsEdsl/CodeGen/C.hs
+++ haskell/library/src/EcsEdsl/CodeGen/C.hs
@@ -15,6 +15,13 @@ data CodeGenState =
     [String]                         -- ^ Current lines of code (backwards)
     (M.Map String ComponentTypeDecl) -- ^ Avaliable Component Types
 
+stateInit :: CodeGenState
+stateInit =
+  CodeGenState
+    0
+    []
+    M.empty
+
 newtype CodeGenT m a = CodeGenT (ReaderT Program (StateT CodeGenState m) a)
 
 unCodeGenT :: forall m a. CodeGenT m a -> ReaderT Program (StateT CodeGenState m) a
@@ -36,6 +43,9 @@ stateGetFreshId (CodeGenState freshId _ _) = freshId
 stateSetFreshId :: Int -> CodeGenState -> CodeGenState
 stateSetFreshId freshId (CodeGenState _ lines componentTypes) = CodeGenState freshId lines componentTypes
 
+stateGetLines :: CodeGenState -> [String]
+stateGetLines (CodeGenState _ lines _) = lines
+
 stateModLines :: ([String] -> [String]) -> CodeGenState -> CodeGenState
 stateModLines f (CodeGenState freshId lines componentTypes) = CodeGenState freshId (f lines) componentTypes
 
@@ -51,3 +61,15 @@ freshVarId = do
   let freshId' = freshId + 1
   CodeGenT $ modify $ stateSetFreshId freshId'
   return freshId
+
+compileToC :: forall m. (Monad m) => Program -> m [String]
+compileToC program = do
+  lines <- evalStateT (runReaderT (unCodeGenT $ compile >> (CodeGenT $ gets stateGetLines)) program) stateInit
+  return $ reverse lines
+
+compile :: forall m. (Monad m) => CodeGenT m ()
+compile = do
+  writeComponentTypeStructs
+
+writeComponentTypeStructs :: forall m. (Monad m) => CodeGenT m ()
+writeComponentTypeStructs = return ()
