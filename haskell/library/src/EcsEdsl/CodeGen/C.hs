9290b6a678e05c2e74b6663fd5d069ac9bfe5e80
clinuxrulz <clinuxrulz@gmail.com>
2016-06-20 17:21:05 +1000

    C codegen

--- haskell/library/src/EcsEdsl/CodeGen/C.hs
+++ haskell/library/src/EcsEdsl/CodeGen/C.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
+
 module EcsEdsl.CodeGen.C where
 
 import EcsEdsl.Program
 import EcsEdsl.Declaration
 import Control.Monad.Reader
 import Control.Monad.State
+import Data.Foldable (traverse_)
 import qualified Data.Map.Strict as M
 
 data CodeGenState =
@@ -13,3 +16,38 @@ data CodeGenState =
     (M.Map String ComponentTypeDecl) -- ^ Avaliable Component Types
 
 newtype CodeGenT m a = CodeGenT (ReaderT Program (StateT CodeGenState m) a)
+
+unCodeGenT :: forall m a. CodeGenT m a -> ReaderT Program (StateT CodeGenState m) a
+unCodeGenT (CodeGenT a) = a
+
+instance (Monad m) => Functor (CodeGenT m) where
+  fmap f (CodeGenT m) = CodeGenT $ fmap f m
+
+instance (Monad m) => Applicative (CodeGenT m) where
+  pure a = CodeGenT $ pure a
+  (CodeGenT mf) <*> (CodeGenT ma) = CodeGenT $ mf <*> ma
+
+instance (Monad m) => Monad (CodeGenT m) where
+  (CodeGenT m) >>= f = CodeGenT $ m >>= (unCodeGenT . f)
+
+stateGetFreshId :: CodeGenState -> Int
+stateGetFreshId (CodeGenState freshId _ _) = freshId
+
+stateSetFreshId :: Int -> CodeGenState -> CodeGenState
+stateSetFreshId freshId (CodeGenState _ lines componentTypes) = CodeGenState freshId lines componentTypes
+
+stateModLines :: ([String] -> [String]) -> CodeGenState -> CodeGenState
+stateModLines f (CodeGenState freshId lines componentTypes) = CodeGenState freshId (f lines) componentTypes
+
+addLine :: forall m. (Monad m) => String -> CodeGenT m ()
+addLine line = CodeGenT $ modify $ stateModLines (line :)
+
+addLines :: forall m f. (Monad m, Foldable f) => f String -> CodeGenT m ()
+addLines lines = traverse_ addLine lines
+
+freshVarId :: forall m. (Monad m) => CodeGenT m Int
+freshVarId = do
+  freshId <- CodeGenT $ gets stateGetFreshId
+  let freshId' = freshId + 1
+  CodeGenT $ modify $ stateSetFreshId freshId'
+  return freshId
