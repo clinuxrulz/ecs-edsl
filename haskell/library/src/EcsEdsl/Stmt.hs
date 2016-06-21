e8abf50fc07ea355a0b532645d28f37b08a77934
Clinton Selke <clinuxrulz@gmail.com>
2016-06-21 21:11:12 +1000

    wip

--- haskell/library/src/EcsEdsl/Stmt.hs
+++ haskell/library/src/EcsEdsl/Stmt.hs
@@ -5,6 +5,25 @@ module EcsEdsl.Stmt where
 import EcsEdsl.Expr
 import EcsEdsl.EntitySystemEff
 
-data Stmt next
+import Control.Monad.Free
+
+data StmtF next
   = Eval (forall r. (forall a. (Expr a, a -> next) -> r) -> r)
-  | LaterWithEntitySystem (EntitySystemEff ()) next
+  | WithEntitySystem (EntitySystemEff ()) next
+
+newtype Stmt a = Stmt (Free StmtF a)
+
+unStmt :: forall a. Stmt a -> Free StmtF a
+unStmt (Stmt a) = a
+
+{- TODO: Define Functor instance for StmtF
+instance Functor Stmt where
+  fmap f (Stmt m) = Stmt $ fmap f m
+
+instance Applicative Stmt where
+  pure a = Stmt $ pure a
+  (Stmt mf) <*> (Stmt ma) = Stmt $ mf <*> ma
+
+instance Monad Stmt where
+  (Stmt m) >>= f = Stmt $ m >>= (unStmt . f)
+-}
